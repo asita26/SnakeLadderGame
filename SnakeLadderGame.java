e0b5a38bcc2c1bb49b8a5cbfd08c97c39ee72f2b
asita26 <asita98890@gmail.com>
2023-11-24 03:15:08 +0530

    Adding functionality for play options

--- SnakeLadderGame.java
+++ SnakeLadderGame.java
@@ -7,17 +7,41 @@ public class SnakeAndLadderGame {
         playGame();
     }
 
-   private static void playGame() {
-        int playerPosition = 0; 
+   private static void playGame() 
+   {
+    int playerPosition = 0; // Starting position
 
-        // Logic for implementation of game 
+    
+        int diceNumber = rollDice();
+        System.out.println("Dice Roll: " + diceNumber);
+        int option = new Random().nextInt(3);
+        
+        switch (option) {
+            case 0:
+                System.out.println("No Play. Player stays at position " + playerPosition);
+                break;
+            case 1:
+                playerPosition += diceNumber;
+                System.out.println("Ladder: Player will go ahead " + diceNumber + " steps to " + playerPosition);
+                break;
+            case 2:
+                playerPosition -= diceNumber;
+                System.out.println("Snake: Player will move back by " + diceNumber + " steps to " + playerPosition);
+                break;
+        }
 
+        System.out.println("Player Position: " + playerPosition);
         System.out.println("Final Player Position: " + playerPosition);
+
+    
+}
+
+    private static int rollDice() 
+    {
+      Random random = new Random();
+      return random.nextInt(6) + 1; // Returns a random number between 1 and 6
     }
 
-    private static int rollDice() {
-    Random random = new Random();
-    return random.nextInt(6) + 1; // Returns a random number between 1 and 6
 }
 
-}
+    
